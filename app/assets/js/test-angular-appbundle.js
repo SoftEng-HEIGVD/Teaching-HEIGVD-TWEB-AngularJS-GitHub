05598b8fc5bd927a942250852dfd58d5380fab89
edri <miguel.santamaria@heig-vd.ch>
2018-01-18 16:14:56 +0100

    grunt build

---
+++ app/assets/js/test-angular-appbundle.js
@@ -0,0 +1,653 @@
+/*!
+* test-angular - v0.0.1 - MIT LICENSE 2018-01-18. 
+* @author Miguel Santamaria
+*/
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc index
+	 * @name app
+	 * @description
+	 * # app
+	 *
+	 * Main modules of the application.
+	 */
+
+	angular.module('test-angular', [
+		'ngResource',
+		'ngAria',
+		 'ngMaterial',
+		'ngMdIcons',
+		'ngCookies',
+		'ngAnimate',
+		'ngSanitize',
+		'ui.router',
+		'home',
+		'rest',
+	]);
+
+})();
+
+(function () {
+	'use strict';
+
+	/**
+	 * @ngdoc configuration file
+	 * @name app.config:config
+	 * @description
+	 * # Config and run block
+	 * Configutation of the app
+	 */
+
+
+	angular
+		.module('test-angular')
+		.config(configure)
+		.run(runBlock);
+
+	configure.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', '$mdThemingProvider'];
+
+	function configure($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $mdThemingProvider) {
+
+		$locationProvider.hashPrefix('!');
+
+		// This is required for Browser Sync to work poperly
+		$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+		
+		$urlRouterProvider
+			.otherwise('/dashboard');
+
+		
+		$mdThemingProvider.theme('input');
+		
+	}
+
+	runBlock.$inject = ['$rootScope'];
+
+	function runBlock($rootScope) {
+		'use strict';
+
+		console.log('AngularJS run() function...');
+	}
+
+
+})();
+
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.module:homeModule
+	* @description
+	* # homeModule
+	* Module of the app
+	*/
+
+	angular.module('home', []);
+})();
+
+(function () {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name app.module:restModule
+	 * @description
+	 * # restModule
+	 * Module of the app
+	 */
+
+  	angular.module('rest', []);
+
+})();
+
+'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.route:HomeRoute
+	* @description
+	* # HomeRoute
+	* Route of the app
+	*/
+
+angular.module('test-angular')
+	.config(['$stateProvider', function ($stateProvider) {
+		$stateProvider
+			
+			.state('home', {
+				url: '',
+				abstract: true,
+				templateUrl: 'app/modules/home/home.html',
+				controller: 'HomeCtrl',
+				controllerAs: 'vm'
+			})
+			.state('home.dashboard', {
+				url:'/dashboard',
+				templateUrl: 'app/modules/home/dashboard.html'
+			});
+			
+	}]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name app.route:restRoute
+ * @description
+ * # restRoute
+ * Route of the app
+ */
+
+angular.module('rest')
+	.config(['$stateProvider', function ($stateProvider) {
+		
+		$stateProvider
+			.state('home.rest', {
+				url:'/rest',
+				templateUrl: 'app/modules/rest/rest.html',
+				controller: 'RestCtrl',
+				controllerAs: 'vm'
+			});
+
+		
+	}]);
+
+(function () {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.controller:HomeCtrl
+	* @description
+	* # HomeCtrl
+	* Controller of the app
+	*/
+
+	angular
+		.module('test-angular')
+		.controller('HomeCtrl', Home);
+
+	Home.$inject = ['homeService'];
+
+	/*
+	* recommend
+	* Using function declarations
+	* and bindable members up top.
+	*/
+
+	function Home(homeService) {
+		/*jshint validthis: true */
+		var vm = this;
+		vm.title = "Hello, test-angular!";
+		vm.version = "1.0.0";
+		vm.listFeatures = homeService.getFeaturesList();
+
+	}
+
+})();
+
+(function () {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.controller:LayoutCtrl
+	* @description
+	* # LayoutCtrl
+	* Controller of the app
+	*/
+
+	angular
+		.module('test-angular')
+		.controller('LayoutCtrl', Layout);
+
+	Layout.$inject = ['$mdSidenav', '$cookies', '$state', '$mdToast', '$mdDialog'];
+
+	/*
+	* recommend
+	* Using function declarations
+	* and bindable members up top.
+	*/
+
+	function Layout($mdSidenav, $cookies, $state, $mdToast, $mdDialog ) {
+		/*jshint validthis: true */
+		var vm = this;
+
+		vm.toggleSidenav = function (menuId) {
+			$mdSidenav(menuId).toggle();
+		};
+
+		vm.changePassword = function () {
+			$mdToast.show(
+				$mdToast.simple()
+				.content('Password clicked!')
+				.position('top right')
+				.hideDelay(2000)
+			);
+		};
+
+		vm.changeProfile = function (ev) {
+			$mdDialog.show({
+				controller: DialogController,
+				templateUrl: 'tabDialog.tmpl.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose:true
+			})
+			.then(function(answer) {
+				$mdToast.show(
+					$mdToast.simple()
+					.content('You said the information was "' + answer + '".')
+					.position('top right')
+					.hideDelay(2000)
+				);
+
+			}, function() {
+				$mdToast.show(
+					$mdToast.simple()
+					.content('You cancelled the dialog.')
+					.position('top right')
+					.hideDelay(2000)
+				);
+			});
+
+			function DialogController($scope, $mdDialog) {
+				$scope.hide = function() {
+					$mdDialog.hide();
+				};
+
+				$scope.cancel = function() {
+					$mdDialog.cancel();
+				};
+
+				$scope.answer = function(answer) {
+					$mdDialog.hide(answer);
+				};
+			}
+		};
+
+
+		vm.logOut = function () {
+
+			alert('Implement your Function Here');
+			// $cookies.put('dev_appserver_login', ' ');
+			//$state.go('out', {}, {reload: true});
+
+		};
+
+		var originatorEv;
+		vm.openMenu = function ($mdOpenMenu, ev) {
+			originatorEv = ev;
+			$mdOpenMenu(ev);
+		};
+
+	}
+
+})();
+
+(function () {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.controller:SidenavCtrl
+	* @description
+	* # SidenavCtrl
+	* Controller of the app
+	*/
+	angular
+		.module('test-angular')
+		.controller('SidenavCtrl', SidenavCtrl)
+		.controller('SettingsCtrl', SettingsCtrl);
+
+	// Injecting Denpendencies
+
+	SidenavCtrl.$inject = ['$mdSidenav', '$state', '$mdBottomSheet', '$mdToast', 'MenuService', '$scope'];
+	SettingsCtrl.$inject = ['$mdBottomSheet'];
+
+	/*
+	* recommend
+	* Using function declarations
+	* and bindable members up top.
+	*/
+
+	function SidenavCtrl($mdSidenav, $state, $mdBottomSheet, $mdToast, MenuService, $scope) {
+		/*jshint validthis: true */
+		var vm = this;
+
+		vm.toggleSidenav = function (menuId) {
+			$mdSidenav(menuId).toggle();
+		};
+
+		vm.closeSidenav = function() {
+			$mdSidenav('left').close();
+		};
+
+		// Close menu on small screen after click on menu item.
+		// Only use $scope in controllerAs when necessary; for example, publishing and subscribing events using $emit, $broadcast, $on or $watch.
+		$scope.$on('$stateChangeSuccess', vm.closeSidenav);
+
+		vm.menu = MenuService.listMenu();
+
+		vm.admin = [
+			{
+				link: 'showListBottomSheet($event)',
+				title: 'Settings',
+				icon: 'settings'
+			}
+		];
+
+		vm.navigateTo = function (target) {
+
+			var page = target;
+
+			$state.go(page);
+
+		};
+
+		vm.showSettingsBottom = function ($event) {
+			vm.alert = '';
+			$mdBottomSheet.show({
+				template: '<md-bottom-sheet class="md-grid" layout="column" ng-cloak><div layout="row" layout-align="center center"><h4>With clickOutsideToClose option, drag down or press ESC to close</h4></div><md-list flex layout="row" layout-align="center center"><md-list-item ng-repeat="item in vm.items"><md-button class="md-grid-item-content" ng-click="vm.listItemClick($index)"><md-icon class="md-48">{{item.icon}}</md-icon><div class="md-grid-text"> {{ item.name }} </div></md-button></md-list-item></md-list></md-bottom-sheet>',
+				controller: 'SettingsCtrl',
+				controllerAs: 'vm',
+				targetEvent: $event
+			}).then(function (clickedItem) {
+				$mdToast.show(
+					$mdToast.simple()
+					.content(clickedItem.name + ' clicked!')
+					.position('top right')
+					.hideDelay(2000)
+				);
+			});
+		};
+
+	}
+
+	function SettingsCtrl($mdBottomSheet) {
+		/*jshint validthis: true */
+		var vm = this;
+
+		vm.items = [
+			{name: 'Roles', icon: 'assignment_ind'},
+			{name: 'Notes', icon: 'speaker_notes'},
+			{name: 'Tasks', icon: 'view_list'},
+			{name: 'Inbox', icon: 'inbox'}
+		];
+
+		vm.listItemClick = function ($index) {
+			var clickedItem = vm.items[$index];
+			$mdBottomSheet.hide(clickedItem);
+		};
+	}
+
+})();
+
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.controller:restCtrl
+	* @description
+	* # restCtrl
+	* Controller of the app
+	*/
+
+  	angular
+			.module('rest')
+			.controller('RestCtrl', RestCtrl);
+
+		RestCtrl.$inject = ['$scope', 'restService'];
+
+		/*
+		* recommend
+		* Using function declarations
+		* and bindable members up top.
+		*/
+
+		function RestCtrl($scope, restService) {
+			/*jshint validthis: true */
+			var vm = this;
+
+			vm.error = null;
+			vm.repositoriesList = ['reactjs/redux', 'facebook/react'];
+			vm.selectedRepository = vm.repositoriesList[0];
+			vm.maximumNumberOfPages = 1;
+			vm.isFetching = false;
+			vm.pullRequests = [];
+			
+			vm.resetError = function() {
+				vm.error = null;
+			}
+
+			vm.addRepositoryToList = function() {
+				vm.resetError();
+
+				if (vm.newRepository) {
+					vm.repositoriesList.push(vm.newRepository);
+					vm.selectedRepository = vm.repositoriesList[vm.repositoriesList.length - 1];
+					vm.newRepository = null;
+					vm.fetchPullRequests();
+				} else {
+					vm.error = 'Please give a valid repository name.';
+				}
+			}
+
+			vm.handleInputKeyPress = function(event) {
+				if ((event.which || event.keyCode) === 13) {
+					vm.addRepositoryToList();
+				}
+			}
+
+			vm.fetchPullRequests = function() {
+				vm.resetError();
+
+				vm.isFetching = true;
+				vm.pullRequests = [];
+
+				restService
+					.executeGetRequest(`https://api.github.com/repos/${vm.selectedRepository}/pulls?state=all`)
+					.then((data) => {
+						vm.isFetching = false;
+
+						if (data.status === 0) {
+							vm.error = 'the selected repository was not found.';
+						} else if (data.status === 1) {
+							vm.pullRequests = data.response.data;
+							vm.lastUpdated = new Date().toLocaleTimeString();
+						}
+
+						// Used to refresh the DOM elements after the new value of the pull requests' array,
+						// since we need to iterate it again with ng-repeat.
+						$scope.$apply();
+					});
+			}
+
+			vm.fetchPullRequests();
+		}
+
+})();
+
+(function () {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.service:homeService
+	* @description
+	* # homeService
+	* Service of the app
+	*/
+
+	angular.module('test-angular')
+		.factory('homeService', homeService);
+
+	homeService.$inject = ['$http'];
+
+	function homeService($http) {
+
+		var list = [
+			{"feature": "Implemented Best Practices, following: John Papa's Guide"},
+			{"feature": "Using Controller AS syntax"},
+			{"feature": "Wrap Angular components in an Immediately Invoked Function Expression (IIFE)"},
+			{"feature": "Declare modules without a variable using the setter syntax"},
+			{"feature": "Using named functions"},
+			{"feature": "Including Unit test with Karma"},
+			{"feature": "Including UI options for Bootstrap or Angular-Material"},
+			{"feature": "Including Angular-Material-Icons for Angular-Material UI"},
+			{"feature": "Dynamic Menu generator for both themes"},
+			{"feature": "Grunt task for Production and Development"}
+		];
+
+		return {
+			getFeaturesList: getFeaturesList
+		};
+
+		function getFeaturesList() {
+			return list;
+		}
+
+	}
+
+})();
+
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name app.service:menuService
+	 * @description
+	 * # menuService
+	 * Service of the app
+	 */
+
+  	angular
+		.module('test-angular')
+		.factory('MenuService', Menu);
+		// Inject your dependencies as .$inject = ['$http', 'someSevide'];
+		// function Name ($http, someSevide) {...}
+
+		Menu.$inject = ['$http'];
+
+		function Menu ($http) {
+
+			var menu = [
+				
+					{
+						link: 'rest',
+							name: 'Rest'
+					},
+			    
+		  	];
+
+			return {
+				listMenu: function () {
+					return menu;
+				}
+		  	}
+
+		}
+
+})();
+
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name app.service:menuService
+	 * @description
+	 * # menuService
+	 * Service of the app
+	 */
+
+  	angular
+		.module('test-angular')
+		.factory('MenuService', Menu);
+		// Inject your dependencies as .$inject = ['$http', 'someSevide'];
+		// function Name ($http, someSevide) {...}
+
+		Menu.$inject = ['$http'];
+
+		function Menu ($http) {
+
+			var menu = [
+				
+					{
+						link: 'rest',
+							name: 'Rest'
+					},
+			    
+		  	];
+
+			return {
+				listMenu: function () {
+					return menu;
+				}
+		  	}
+
+		}
+
+})();
+
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name app.service:restService
+	 * @description
+	 * # restService
+	 * Service of the app
+	 */
+
+  	angular
+		.module('rest')
+		.factory('restService', restService);
+		// Inject your dependencies as .$inject = ['$http', 'someSevide'];
+		// function Name ($http, someSevide) {...}
+
+		restService.$inject = ['$http'];
+
+		function restService ($http) {
+			return {
+				executeGetRequest: executeGetRequest,
+			};
+
+			function executeGetRequest(urlString) {
+				return new Promise((resolve) => {
+					console.log(`Fetching ${urlString}...`);
+	
+					// Simple GET request example:
+					$http({
+						method: 'GET',
+						url: urlString,
+						headers: {
+							'Accept': 'application/vnd.github.v3+json',
+						}
+					}).then(function successCallback(response) {
+							// this callback will be called asynchronously
+							// when the response is available
+							resolve({
+								status: 1, 
+								response,
+							});
+						}, function errorCallback(response) {
+							// called asynchronously if an error occurs
+							// or server returns response with an error status.
+							resolve({
+								status: 0, 
+								response,
+							});
+						});
+				});
+			}
+		}
+
+})();
